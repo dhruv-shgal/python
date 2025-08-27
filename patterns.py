1a8cf691b15aeb4f69e7ac6d512c8d9976eeebba
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-08-27 21:40:31 +0530

    Initial commit

--- patterns.py
+++ patterns.py
@@ -1,9 +1,31 @@
 n=int(input("enter the side of square"))
-temp =0
-for i in range(n):
-    for j in range(i):
-        temp=temp+1
-        print(temp, end=" ")
-    print()    
+
+# DIAMOND PATTERN
+# temp =0
+# for i in range(n):
+#     for j in range(n-i-1):
+#         print(" ",end="")
+#     for k in range(2*i+1):
+#         print("*",end="") 
+#     print()    
+    
+# for i in range(n - 2, -1, -1):   # start from n-2 (to avoid repeating middle row)
+#     # spaces
+#     for j in range(n - i - 1):
+#         print(" ", end="")
+#     # stars
+#     for k in range(2 * i + 1):
+#         print("*", end="")
+#     print() 
+
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end="")
+    for k in range(i):
+        print("*",end="")
+    print()     
+
+               
+    
 
        
