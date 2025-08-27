30bdc31927c475adc280cd2795b980965fe1e2ae
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-08-27 22:03:32 +0530

    Initial commit

--- patterns.py
+++ patterns.py
@@ -1,7 +1,7 @@
 n=int(input("enter the side of square"))
 
 # DIAMOND PATTERN
-# temp =0
+
 # for i in range(n):
 #     for j in range(n-i-1):
 #         print(" ",end="")
@@ -18,14 +18,24 @@ n=int(input("enter the side of square"))
 #         print("*", end="")
 #     print() 
 
-for i in range(1,n+1):
-    for j in range(n-i):
-        print(" ",end="")
-    for k in range(i):
-        print("*",end="")
-    print()     
+# for i in range(1,n+1):
+#     for j in range(n-i):
+#         print(" ",end="")
+#     for k in range(i):
+#         print("*",end="")
+#     print()     
 
-               
-    
+# for i in range(n):
+#     for j in range(i):
+#         print(" ",end="")
+#     for j in range(2*(n-i)-1):
+#         print("*",end="")
+#     print()
+# for i in range(n-2,-1,-1):
+#     for j in range(i):
+#         print(" ",end="")
+#     for j in range(2*(n-i)-1):
+#         print("*",end="")
+#     print()
 
-       
+
