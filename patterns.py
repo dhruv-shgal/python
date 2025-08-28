212b4b16dd9c2c29276091dbc4e956547ca89db0
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-08-28 12:38:30 +0530

    Initial commit

--- patterns.py
+++ patterns.py
@@ -39,3 +39,20 @@ n=int(input("enter the side of square"))
 #     print()
 
 
+# for i in range(n):
+#     for j in range(i):
+#         if j==0 or j==i-1 or i==n-1:
+#             print("*",end="")
+#         else:
+#             print(" ",end="")
+#     print()
+
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end="")
+    for k in range(i):
+        if k==0 or k==i-1 or i==n:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()     
