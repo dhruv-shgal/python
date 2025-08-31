7471eba267035ab70cc6dadf6d2119916f8d3174
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-08-31 18:21:07 +0530

    Initial commit

--- built_in.py
+++ built_in.py
@@ -30,11 +30,38 @@
 #     return (cnt,cut)        
 # print(count_even_odd([1,2,3,4,5,6,7,8,9]))
 
-def max_consecutive_difference(lst):
-    max_diff=0
-    for i in range(1,len(lst)):
-        max_diff=max(abs(lst[i]-lst[i-1]),max_diff)
-    return max_diff
+# def max_consecutive_difference(lst):
+#     max_diff=0
+#     for i in range(1,len(lst)):
+#         max_diff=max(abs(lst[i]-lst[i-1]),max_diff)
+#     return max_diff
 
-lst = [1, 10, 3, 7, 5]
-print(max_consecutive_difference(lst))  
+# lst = [1, 10, 3, 7, 5]
+# print(max_consecutive_difference(lst))  
+
+# def merge_sorted_list(lst1,lst2):
+#     lst1.extend(lst2)
+#     return sorted(lst1)
+# lst1 = [1, 3, 5]
+# lst2 = [2, 4, 6]
+# mai=merge_sorted_list(lst1,lst2)
+# final=list(dict.fromkeys(mai))
+# print(final)
+
+# def rotate_array(arr,d):
+#     # arr=arr[d:]+arr[:d]
+#     # d = d % len(arr)
+#     arr = arr[-d:] + arr[:-d]
+#     return arr
+# arr=[1, 2, 3, 4, 5]
+# d=2
+# print(rotate_array(arr,d))
+
+def merge_dict(keys, values,main):
+    merged_dict = {**keys, **values,**main}
+    return merged_dict
+
+keys = {'a': 1, 'b': 2}
+values = {'c': 3, 'd': 4}
+main={'e': 5, 'f': 6}
+print(merge_dict(keys, values,main))
