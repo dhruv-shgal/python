e023a2e71cca4d2593dff888dc33a8ad3116708c
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-09-02 13:57:12 +0530

    Initial commit

--- strings.py
+++ strings.py
@@ -74,13 +74,13 @@
 # t = "rat"
 # print(is_anagram(s, t))
 
-def is_subsequence(s, t):
-    i,j=0,0
-    while i<len(s) and j<len(t):
-        if s[i]==t[j]:
-            i+=1
-        j+=1
-    return i==len(s)
-s = "abcde"
-t = "aec"
-print(is_subsequence(s, t))
+# def is_subsequence(s, t):
+#     i,j=0,0
+#     while i<len(s) and j<len(t):
+#         if s[i]==t[j]:
+#             i+=1
+#         j+=1
+#     return i==len(s)
+# s = "abcde"
+# t = "aec"
+# print(is_subsequence(s, t))
