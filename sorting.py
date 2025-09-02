835a14f338afafdaca33d3d1aa70afae154f407f
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-09-02 15:19:29 +0530

    Initial commit

---
+++ sorting.py
@@ -0,0 +1,17 @@
+#BUBBLE SORT
+# l1=[5,4,3,2,1]
+# for j in range(len(l1)-1):
+#     for i in range(1,len(l1)-j):
+#         if l1[i]<l1[i-1]:
+#             l1[i],l1[i-1]=l1[i-1],l1[i]
+# print(l1)
+
+#SELECTION SORT
+lst1=[9,13,20,24,46,52]
+for i in range(len(lst1)-2):
+    mini=i
+    for  j in range(i,len(lst1)):
+        if lst1[j]<lst1[mini]:
+            mini=j
+    lst1[i],lst1[mini]=lst1[mini],lst1[i]
+print(lst1)    
