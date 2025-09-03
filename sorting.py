45bbbf1061d71b2cd57a50fa474df4594ead7e91
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-09-03 13:30:48 +0530

    Initial commit

--- sorting.py
+++ sorting.py
@@ -7,11 +7,23 @@
 # print(l1)
 
 #SELECTION SORT
-lst1=[9,13,20,24,46,52]
-for i in range(len(lst1)-2):
-    mini=i
-    for  j in range(i,len(lst1)):
-        if lst1[j]<lst1[mini]:
-            mini=j
-    lst1[i],lst1[mini]=lst1[mini],lst1[i]
-print(lst1)    
+# lst1=[9,13,20,24,46,52]
+# for i in range(len(lst1)-2):
+#     mini=i
+#     for  j in range(i,len(lst1)):
+#         if lst1[j]<lst1[mini]:
+#             mini=j
+#     lst1[i],lst1[mini]=lst1[mini],lst1[i]
+# print(lst1)    
+
+def insertion_sort(lst1):
+    for i in range(len(lst1)):
+        j=i
+        while(j>0 and lst1[j-1]>lst1[j]):
+            lst1[j-1],lst1[j]=lst1[j],lst1[j-1]
+            j-=1
+    return lst1        
+
+
+lst1=[12 ,25 ,11 ,34 ,90,22]
+print(insertion_sort(lst1))
