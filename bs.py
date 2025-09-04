58da0daaf7052b47a5f5f8a19a3f10faa5cb763e
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-09-04 22:03:14 +0530

    Initial commit

--- bs.py
+++ bs.py
@@ -34,39 +34,56 @@
 # target='d'
 # print(next_greatest_letter(letters,target))
 
-def searchRange(nums, target):
-    l=0
-    h=len(nums)-1
-    res=-1
-    while(l<=h):
-        mid=l+(h-l)//2
-        if nums[mid]==target:
-            res=mid
-            h=mid-1
-        elif nums[mid]<target:
-            l=mid+1
-        else:
-            h=mid-1
-    return res        
-def last_index(nums,target):
-    l=0
-    h=len(nums)-1
-    res1=-1
-    while(l<=h):
-        mid=l+(h-l)//2
-        if nums[mid]==target:
-            res1=mid
-            l=mid+1
-        elif nums[mid]<target:
-            l=mid+1
-        else:
-            h=mid-1
-    return res1  
+# def searchRange(nums, target):
+#     l=0
+#     h=len(nums)-1
+#     res=-1
+#     while(l<=h):
+#         mid=l+(h-l)//2
+#         if nums[mid]==target:
+#             res=mid
+#             h=mid-1
+#         elif nums[mid]<target:
+#             l=mid+1
+#         else:
+#             h=mid-1
+#     return res        
+# def last_index(nums,target):
+#     l=0
+#     h=len(nums)-1
+#     res1=-1
+#     while(l<=h):
+#         mid=l+(h-l)//2
+#         if nums[mid]==target:
+#             res1=mid
+#             l=mid+1
+#         elif nums[mid]<target:
+#             l=mid+1
+#         else:
+#             h=mid-1
+#     return res1  
     
 
 
-nums=[5,7,7,8,8,10]
-target=8
-first=searchRange(nums,target)
-last=last_index(nums,target)
-print(f"Number = {target}, the first index is {first} and the last index is {last}")
+# nums=[5,7,7,8,8,10]
+# target=8
+# first=searchRange(nums,target)
+# last=last_index(nums,target)
+# print(f"Number = {target}, the first index is {first} and the last index is {last}")
+
+def findMin(nums):
+    l=0
+    h=len(nums)-1
+    ans=float('inf')
+    while(l<=h):
+        mid=l+(h-l)//2
+        if nums[mid]>=nums[l]:
+            ans=min(ans,nums[l])
+            l=mid+1
+        else:
+            ans=min(ans,nums[mid])
+            h=mid-1    
+    return ans
+
+nums=[4,5,6,7,0,1,2]    
+print(findMin(nums))
