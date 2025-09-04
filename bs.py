657851d040278b0a49db732141da83e5f8459290
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-09-04 22:09:49 +0530

    Initial commit

--- bs.py
+++ bs.py
@@ -71,19 +71,35 @@
 # last=last_index(nums,target)
 # print(f"Number = {target}, the first index is {first} and the last index is {last}")
 
-def findMin(nums):
+# def findMin(nums):
+#     l=0
+#     h=len(nums)-1
+#     ans=float('inf')
+#     while(l<=h):
+#         mid=l+(h-l)//2
+#         if nums[mid]>=nums[l]:
+#             ans=min(ans,nums[l])
+#             l=mid+1
+#         else:
+#             ans=min(ans,nums[mid])
+#             h=mid-1    
+#     return ans
+
+# nums=[4,5,6,7,0,1,2]    
+# print(findMin(nums))
+
+def search(nums, target):
     l=0
     h=len(nums)-1
-    ans=float('inf')
     while(l<=h):
         mid=l+(h-l)//2
-        if nums[mid]>=nums[l]:
-            ans=min(ans,nums[l])
+        if nums[mid]==target:
+            return mid
+        elif nums[mid]>target:
             l=mid+1
         else:
-            ans=min(ans,nums[mid])
-            h=mid-1    
-    return ans
+            h=mid-1
 
-nums=[4,5,6,7,0,1,2]    
-print(findMin(nums))
+nums = [4, 5, 6, 7, 0, 1, 2]   
+target=0 
+print(search(nums.target))
