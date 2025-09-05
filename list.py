4adcf11890bd5d52c6164f11ebe3693bb3f5e244
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-09-05 11:41:33 +0530

    Initial commit

--- list.py
+++ list.py
@@ -1,7 +1,18 @@
-def is_palindrome(lst):
-    return lst==lst[::-1]
+# def is_palindrome(lst):
+#     return lst==lst[::-1]
 
 
 
-lst=[7,8,9,8,6]    
-print(is_palindrome(lst))
+# lst=[7,8,9,8,6]    
+# print(is_palindrome(lst))
+
+def rotate_array(arr,d):
+    arr=arr[d:]+arr[:d]
+    d = d % len(arr)
+    arr = arr[-d:] + arr[:-d]
+    return arr
+arr=[1, 2, 3, 4, 5]
+d=2
+print(rotate_array(arr,d))
+
+
