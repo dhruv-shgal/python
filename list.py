2ea3cd45230f2411ab02f7ae88e938a86d0b5954
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-09-05 12:22:28 +0530

    Initial commit

--- list.py
+++ list.py
@@ -6,14 +6,31 @@
 # lst=[7,8,9,8,6]    
 # print(is_palindrome(lst))
 
-def rotate_array(arr,d):
-    # arr=arr[d:]+arr[:d]
-    # d = d % len(arr)
-    # arr = arr[-d:] + arr[:-d]
-    # return arr
-    return arr
-arr=[1, 2, 3, 4, 5]
-d=2
-print(rotate_array(arr,d))
+# def rotate_array(arr,d):
+#     # arr=arr[d:]+arr[:d]
+#     # d = d % len(arr)
+#     # arr = arr[-d:] + arr[:-d]
+#     # return arr
+#     return arr
+# arr=[1, 2, 3, 4, 5]
+# d=2
+# print(rotate_array(arr,d))
 
+def plusone(lst):
+    lst=lst[::-1]
+    one,i=1,0
+    while one:
+        if i<len(lst):
+            if lst[i]==9:
+                lst[i]=0
+            else:    
+                lst[i]+=1
+                one=0
+        else:
+            lst.append(1)
+            one=0
+        i+=1    
+    return lst[::-1]                
 
+lst=[5,4,5,9]
+print(plusone(lst))
