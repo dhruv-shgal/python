da909c19c2a637cce057b0eae7de5358a0cbf699
Dhruv sehgal <160312756+dhruv-shgal@users.noreply.github.com>
2025-09-07 17:22:52 +0530

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -57,7 +57,7 @@ n=int(input("enter the side of square"))
 #             print(" ",end="")
 #     print()     
 
-for i in range(n):
+#for i in range(n):
 
     for j in range(n-i-1):
         print(" ",end="")
