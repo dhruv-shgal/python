fcff3dbe12c9d4e1db4cd82981e3a94c897b74b7
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-09-14 21:53:59 +0530

    Initial commit

--- revision.py
+++ revision.py
@@ -1,16 +1,21 @@
 #Anagram
-def anagram(s,t):
+# def anagram(s,t):
     
-    count1,count2={},{}
-    for i in range(len(s)):
-        count1[s[i]]=1+count1.get(s[i],0)
-        count2[t[i]]=1+count2.get(t[i],0)
-    for c in count1:
-        if count1[c]!=count2.get(c,0):
-            return False
-    return True
+#     count1,count2={},{}
+#     for i in range(len(s)):
+#         count1[s[i]]=1+count1.get(s[i],0)
+#         count2[t[i]]=1+count2.get(t[i],0)
+#     for c in count1:
+#         if count1[c]!=count2.get(c,0):
+#             return False
+#     return True
 
-s = "listen"
-t = "silent"
-print(f"the string is {anagram(s,t)}")
-    
+# s = "listen"
+# t = "silent"
+# print(f"the string is {anagram(s,t)}")
+
+
+# def reverse_sent(s):
+
+s="the sky is blue"
+print(" ".join(s.split()[::-1]))    
