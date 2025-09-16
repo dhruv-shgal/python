4a727bb14301df1331b597ce54097df446695656
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-09-16 22:54:19 +0530

    Initial commit

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
 
 
 
-# def rounds_lift(n,c) :
-#     if n//c==0:
-#         return n//c
-#     else: 
-#         return n//c + 1
-# print(rounds_lift(7,4)) 
+def rounds_lift(n,c) :
+    if n//c==0:
+        return n//c
+    else: 
+        return n//c + 1
+print(rounds_lift(7,4)) 
