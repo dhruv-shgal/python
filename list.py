79e478b0927472e63715baec8cf8ac852b4d23cb
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-09-16 22:54:46 +0530

    Initial commit

--- list.py
+++ list.py
@@ -1,103 +1,103 @@
-# def is_palindrome(lst):
-#     return lst==lst[::-1]
+def is_palindrome(lst):
+    return lst==lst[::-1]
 
 
 
-# lst=[7,8,9,8,6]    
-# print(is_palindrome(lst))
+lst=[7,8,9,8,6]    
+print(is_palindrome(lst))
 
-# def rotate_array(arr,d):
-#     # arr=arr[d:]+arr[:d]
-#     # d = d % len(arr)
-#     # arr = arr[-d:] + arr[:-d]
-#     # return arr
-#     return arr
-# arr=[1, 2, 3, 4, 5]
-# d=2
-# print(rotate_array(arr,d))
+def rotate_array(arr,d):
+    # arr=arr[d:]+arr[:d]
+    # d = d % len(arr)
+    # arr = arr[-d:] + arr[:-d]
+    # return arr
+    return arr
+arr=[1, 2, 3, 4, 5]
+d=2
+print(rotate_array(arr,d))
 
-# def plusone(lst):
-#     lst=lst[::-1]
-#     one,i=1,0
-#     while one:
-#         if i<len(lst):
-#             if lst[i]==9:
-#                 lst[i]=0
-#             else:    
-#                 lst[i]+=1
-#                 one=0
-#         else:
-#             lst.append(1)
-#             one=0
-#         i+=1    
-#     return lst[::-1]                
+def plusone(lst):
+    lst=lst[::-1]
+    one,i=1,0
+    while one:
+        if i<len(lst):
+            if lst[i]==9:
+                lst[i]=0
+            else:    
+                lst[i]+=1
+                one=0
+        else:
+            lst.append(1)
+            one=0
+        i+=1    
+    return lst[::-1]                
 
-# lst=[5,9,9,9]
-# print(plusone(lst))
-
-
-# def find_missing_number(nums):
-#     xor1,xor2=0,0
-#     n=len(nums)-1
-#     for i in range(n):
-#         xor2=xor2^nums[i]
-#         xor1=xor1^(i+1)
-#     xor1=xor1^n
-#     return xor1^xor2    
-# nums = [3, 0, 1]
-# print(find_missing_number(nums))
-
-# def is_sorted(arr):
-#     return sorted(arr)==arr
+lst=[5,9,9,9]
+print(plusone(lst))
 
 
-# arr = [1, 2, 3, 4, 5]
-# print(is_sorted(arr))
+def find_missing_number(nums):
+    xor1,xor2=0,0
+    n=len(nums)-1
+    for i in range(n):
+        xor2=xor2^nums[i]
+        xor1=xor1^(i+1)
+    xor1=xor1^n
+    return xor1^xor2    
+nums = [3, 0, 1]
+print(find_missing_number(nums))
+
+def is_sorted(arr):
+    return sorted(arr)==arr
 
 
-# def move_zeroes(nums):
-#     j=0
-#     for i in range(len(nums)):
-#         if nums[i]!=0:
-#            nums[i],nums[j]=nums[j],nums[i]
-#            j+=1
-#     return nums           
-# nums = [0, 1, 0, 3, 12]
-# print(move_zeroes(nums))
+arr = [1, 2, 3, 4, 5]
+print(is_sorted(arr))
 
 
-# def intersection(nums1, nums2):
-#         return list(set(nums1) & set(nums2))
-#         return []
-# nums1 = [1, 65, 5]
-# nums2 = [9, 4, 9,8,4]
-# print(intersection(nums1,nums2))
+def move_zeroes(nums):
+    j=0
+    for i in range(len(nums)):
+        if nums[i]!=0:
+           nums[i],nums[j]=nums[j],nums[i]
+           j+=1
+    return nums           
+nums = [0, 1, 0, 3, 12]
+print(move_zeroes(nums))
 
-# def find_max_consecutive_ones(nums):
-#     cnt=0
-#     max_cnt=0
-#     for i in nums:
-#         if i==1:
-#             cnt+=1
-#             max_cnt=max(cnt,max_cnt)
-#         else:
-#             cnt=0
-#     return cnt
+
+def intersection(nums1, nums2):
+        return list(set(nums1) & set(nums2))
+        return []
+nums1 = [1, 65, 5]
+nums2 = [9, 4, 9,8,4]
+print(intersection(nums1,nums2))
+
+def find_max_consecutive_ones(nums):
+    cnt=0
+    max_cnt=0
+    for i in nums:
+        if i==1:
+            cnt+=1
+            max_cnt=max(cnt,max_cnt)
+        else:
+            cnt=0
+    return cnt
         
 
-# nums = [1, 0, 1, 1, 0, 1, 1, 1, 1]    
-# print(find_max_consecutive_ones(nums))
+nums = [1, 0, 1, 1, 0, 1, 1, 1, 1]    
+print(find_max_consecutive_ones(nums))
 
-# def max_subarray_sum(arr):
-#     currsum=0
-#     maxsum=arr[0]
-#     for i in arr:
-#         if currsum < 0:
-#             currsum=0
-#         currsum+=i
-#         maxsum=max(maxsum,currsum)
-#     return maxsum        
+def max_subarray_sum(arr):
+    currsum=0
+    maxsum=arr[0]
+    for i in arr:
+        if currsum < 0:
+            currsum=0
+        currsum+=i
+        maxsum=max(maxsum,currsum)
+    return maxsum        
 
 
-# arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-# print(max_subarray_sum(arr))
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(max_subarray_sum(arr))
