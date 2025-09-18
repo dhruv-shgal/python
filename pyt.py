316d6a04d1b6fdaa5bb88e4c07b0650b8d2eecfd
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-09-18 12:17:42 +0530

    Initial commit

--- pyt.py
+++ pyt.py
@@ -1,7 +1,14 @@
-a=[1,2,3]
-b=a
-print(b is a)
-print(b==a)
+# a=[1,2,3]
+# b=a
+# print(b is a)
+# print(b==a)
 
-b=[1,2,3]
-print(b is a)
+# b=[1,2,3]
+# print(b is a)
+
+x=5
+def global_(num):
+    # global x
+    print(x)
+    x=num
+print(global_(5))    
