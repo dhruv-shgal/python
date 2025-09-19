954b8db24cf806ae7372864d55aa5f91a52eb5d7
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-09-19 22:25:30 +0530

    Initial commit

--- buy_sell.py
+++ buy_sell.py
@@ -1,15 +1,14 @@
 prices = [7,1,5,3,6,4]
-l=0
-r=1
-maxprofit=0
-while(r<len(prices)):
-    if prices[l] < prices[r]:
-        profit=prices[r]-prices[l]
-        maxprofit=max(profit,maxprofit)
-    else:
-        l+=1
-    r+=1
-print(f"the maximum profit is {maxprofit}")        
+     l,r=0,1
+        maxprofit=0
+        while r<len(prices):
+            if prices[l]<=prices[r]:
+                profit=prices[r]-prices[l]
+                maxprofit=max(profit,maxprofit)
+            else:
+                l=r
+            r+=1
+        return maxprofit            
 
 
        
