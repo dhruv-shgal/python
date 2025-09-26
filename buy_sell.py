15c05adc6d08918d5c337e3e27d43cbf793ef27a
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-09-26 21:07:50 +0530

    Initial commit

--- buy_sell.py
+++ buy_sell.py
@@ -1,16 +1,16 @@
 #maximum profit - buy and sell stocks
 def profit():
-    prices = [7,1,5,3,6,4]
-    l,r=0,1
-    maxprofit=0
-    while r<len(prices):
-                if prices[l]<=prices[r]:
-                    profit=prices[r]-prices[l]
-                    maxprofit=max(profit,maxprofit)
-                else:
-                    l=r
-                r+=1
-    return maxprofit            
+    # prices = [7,1,5,3,6,4]
+    # l,r=0,1
+    # maxprofit=0
+    # while r<len(prices):
+    #             if prices[l]<=prices[r]:
+    #                 profit=prices[r]-prices[l]
+    #                 maxprofit=max(profit,maxprofit)
+    #             else:
+    #                 l=r
+    #             r+=1
+    # return maxprofit            
 
 
        
