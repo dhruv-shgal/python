b37fc8c48c5c0aa0c77f5f673155b1f851f9c2ef
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-09-28 20:39:17 +0530

    Initial commit

---
+++ dsa_rev/bs2.py
@@ -0,0 +1,17 @@
+def no_sorted(arr):
+    l=0
+    n=len(arr)
+    h=len(arr)-1
+    while l<=h:
+        mid=(l+h)//2
+        next=(mid+1)%n
+        prev=(mid+n-1)%n
+        if arr[mid]<=arr[prev] and arr[mid]<=arr[next]:
+            return mid
+        elif arr[l]<=arr[mid]:
+            l=mid+1
+        else:
+            h=mid-1
+    return -1
+arr=[11,12,13,18,2,5,6,8]
+print(no_sorted(arr))
