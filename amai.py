4a75f76c7ba1e3902e757d4abbc30baea2802c90
dhruv-shgal <dhruvsehgal.022@gmail.com>
2025-09-29 13:05:24 +0530

    Initial commit

--- amai.py
+++ amai.py
@@ -1,3 +1,6 @@
 # print('%.5s' %('xylophone'))
-sentence = "Today is Saturaday"
-print(sentence.find('day'))
+# sentence = "Today is Saturaday"
+# print(sentence.find('day'))
+a=-5
+b=10
+print(a.__add__(b))
